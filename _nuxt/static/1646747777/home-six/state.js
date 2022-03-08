58623fd78098f2d9613b5c055572cbcc46275a80
Aldo Gonzalez <aldo.linux@gmail.com>
2022-03-08 12:48:02 -0500

    Generate static test

--- _nuxt/static/1646747777/home-six/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646747777",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhome-six",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},strapi:{url:"http:\u002F\u002F192.168.0.211:1337"},http:{browserBaseURL:b}}}}(null,"\u002F"));
--- _nuxt/static/1646747777/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646747777",layout:"default",error:b,serverRendered:true,routePath:a,config:{_app:{basePath:a,assetsPath:"\u002F_nuxt\u002F",cdnURL:b},strapi:{url:"http:\u002F\u002F192.168.0.211:1337"},http:{browserBaseURL:a}}}}("\u002F",null));
